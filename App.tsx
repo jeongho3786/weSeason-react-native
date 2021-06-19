dc1a67273e4a18a2dfaebe09ba1f53ead958618c
jeongho3786 <jeongho3786@gmail.com>
2021-06-19 17:02:41 +0900

    setting background

--- App.tsx
+++ App.tsx
@@ -4,15 +4,16 @@ import MainContainer from "./main/mainContainer";
 import FooterContainer from "./footer/footerContainer";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <LinearGradient colors={["#87B0FF", "#A3FFF9"]} style={styles.container}>
       <HeaderContainer />
       <MainContainer />
       <FooterContainer />
       <StatusBar style="auto" />
-    </View>
+    </LinearGradient>
   );
 }
 
